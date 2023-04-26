84fd310ff526118566006719fc7588648208a912
Kevin Cruz <kevfaridc@gmail.com>
2023-04-25 19:35:17 -0500

    feat: add title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,6 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <FavoritesProvider>
       <main className={`${inter.className} w-full`}>
+        <Head>
+          <title>Rick And Your Favorites</title>
+        </Head>
         <Header />
         <section
           className={`${inter.className} flex flex-col w-full items-center gap-8 mb-8 relative`}
